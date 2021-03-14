92535c74cc43d9c6febaad7834583f59b5e667e8
lemonaj58 <lemonaj58@gmail.com>
2021-03-13 19:24:27 -0800

    updated problems

--- practiceProblems/debugging.js
+++ practiceProblems/debugging.js
@@ -1,3 +1,19 @@
+//word ladder, they forgot all the semicolins.
+
+let ladder = '';
+
+['head', 'heal', 'teal', 'tell', 'tall', 'tail'].forEach(word => {
+  if (ladder !== '') {
+    ladder += '-';
+  }
+
+  ladder += word;
+});
+
+console.log(ladder);
+
+//reserved keywords
+
 const RESERVED_KEYWORDS = ['break', 'case', 'catch', 'class', 'const', 'continue',
   'debugger', 'default', 'delete', 'do', 'else', 'enum', 'export', 'extends', 'finally',
   'for', 'function', 'if', 'implements', 'import', 'in', 'instanceof', 'interface',
@@ -12,3 +28,6 @@ function isReserved(name) {
 console.log(isReserved('monkey')); // false
 console.log(isReserved('patch'));  // false
 console.log(isReserved('switch')); // should be: true
+// the end of a forEach statement always returned an undefined statement.
+//it also does not end when it is selected as true. meaning it will always be
+// a false statement.
--- practiceProblems/medium1.js
+++ practiceProblems/medium1.js
@@ -52,13 +52,19 @@ rotateRightmostDigits(735291, 5);      // 752913
 rotateRightmostDigits(735291, 6);      // 352917
 
 
-
-function maxRotation(number)) {
+function maxRotation(number) {
   let stringNumber = number.toString();
-  let length = stringNumber.length
-  for (let index = 1; index <= stringNumber.length; index += 1) {
-    console.log(stringNumber.slice(index, length))
+  let length = stringNumber.length;
+  let startingNumber = stringNumber.slice(1, length) + stringNumber[0];
+  let newNumber = '';
+
+  for (let index = 0; index < length; index += 1) {
+    newNumber += startingNumber.slice(0, 1);
+    startingNumber = startingNumber.slice(2, length) + startingNumber[1];
+
+
   }
+  return (Number(newNumber));
 }
 
 maxRotation(735291);          // 321579
@@ -66,3 +72,139 @@ maxRotation(3);               // 3
 maxRotation(35);              // 53
 maxRotation(105);             // 15 -- the leading zero gets dropped
 maxRotation(8703529146);      // 7321609845
+
+//stack machine interpretation
+
+// eslint-disable-next-line max-lines-per-function
+function minilang(string) {
+  let array = string.split(' ');
+
+  let callbackArray = [];
+  let calculation = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    let word = array[index];
+
+    switch (word) {
+      case 'PUSH':
+        callbackArray.push(calculation);
+        break;
+      case 'PRINT':
+        console.log(calculation);
+        break;
+      case 'ADD':
+        calculation += callbackArray.pop();
+        break;
+      case 'SUB':
+        calculation -= callbackArray.pop();
+        break;
+      case 'MULT':
+        calculation *= callbackArray.pop();
+        break;
+      case 'DIV':
+        calculation /= callbackArray.pop();
+        break;
+      case 'POP':
+        callbackArray.pop();
+        break;
+      case 'MOD':
+        Math.floor(calculation /= callbackArray.pop());
+        break;
+    }
+  }
+
+}
+
+minilang('PRINT');
+// 0
+
+minilang('5 PUSH 3 MULT PRINT');
+// 15
+
+minilang('5 PRINT PUSH 3 PRINT ADD PRINT');
+// 5
+// 3
+// 8
+
+minilang('5 PUSH POP PRINT');
+// 5
+
+minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT');
+// 5
+// 10
+// 4
+// 7
+
+minilang('3 PUSH PUSH 7 DIV MULT PRINT');
+// 6
+
+minilang('4 PUSH PUSH 7 MOD MULT PRINT');
+// 12
+
+minilang('-3 PUSH 5 SUB PRINT');
+// 8
+
+minilang('6 PUSH');
+// (nothing is printed because the `program` argument has no `PRINT` commands)
+//word to digit
+let numbers = {
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
+  zero: '0',
+};
+function wordToDigit(string) {
+
+  console.log(string.split(' ').map(word => {
+    if (Object.keys(numbers).includes(word)) {
+      return numbers[word];
+    } else {
+      return word;
+    }
+  }).join(' '));
+}
+
+wordToDigit('Please call me at five five five one two three four. Thanks.');
+// "Please call me at 5 5 5 1 2 3 4. Thanks."
+
+//fibonacci numbers (recursion)
+
+function fibonacci(num) {
+  if (num <= 2) {
+    return 1;
+  }
+  return fibonacci(num - 1) + fibonacci(num - 2);
+
+}
+
+console.log(fibonacci(1));       // 1
+console.log(fibonacci(2));      // 1
+console.log(fibonacci(3));      // 2
+console.log(fibonacci(4));      // 3
+console.log(fibonacci(5));// 5
+console.log(fibonacci(12));      // 144
+console.log(fibonacci(20));      // 6765
+
+//fibonacci numbers procedural
+
+function fibonacci2(num) {
+  let startingNum = 1;
+  let secondNumber = 1;
+  let calculation = 0;
+  for (let index = 3; index < num; index += 1) {
+    calculation = startingNum + secondNumber;
+    secondNumber = startingNum;
+    startingNum = calculation;
+
+  }
+  return startingNum;
+}
+
+fibonacci2(20);       // 6765
+fibonacci2(50);       // 12586269025
+fibonacci2(75);       // 2111485077978050
