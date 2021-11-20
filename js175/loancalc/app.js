fc4be4acb1f68bc4d25b7c18731bb889f0d5e774
lemonaj58 <lemonaj58@gmail.com>
2021-11-20 14:50:24 -0700

    homepage enter your own information w/o query

--- js175/loancalc/app.js
+++ js175/loancalc/app.js
@@ -1,9 +1,10 @@
+/* eslint-disable max-lines-per-function */
 const HTTP = require('http');
 const PORT = 3003;
 const URL = require('url').URL;
 const HANDLEBARS = require('handlebars');
 
-const SOURCE = `
+const LOAN_OFFER_SOURCE = `
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -51,21 +52,21 @@ const SOURCE = `
           <tr>
             <th>Amount:</th>
             <td>
-              <a href='/?amount={{amountDecrement}}&duration={{duration}}'>- $100</a>
+              <a href='/loan-offer?amount={{amountDecrement}}&duration={{duration}}'>- $100</a>
             </td>
             <td>$ {{amount}}</td>
             <td>
-              <a href='/?amount={{amountIncrement}}&duration={{duration}}'>+ $100</a>
+              <a href='/loan-offer?amount={{amountIncrement}}&duration={{duration}}'>+ $100</a>
             </td>
           </tr>
           <tr>
             <th>Duration:</th>
             <td>
-              <a href='/?amount={{amount}}&duration={{durationDecrement}}'>- 1 year</a>
+              <a href='/loan-offer?amount={{amount}}&duration={{durationDecrement}}'>- 1 year</a>
             </td>
             <td>{{duration}} years</td>
             <td>
-              <a href='/?amount={{amount}}&duration={{durationIncrement}}'>+ 1 year</a>
+              <a href='/loan-offer?amount={{amount}}&duration={{durationIncrement}}'>+ 1 year</a>
             </td>
           </tr>
           <tr>
@@ -82,7 +83,74 @@ const SOURCE = `
   </body>
 </html>
 `;
-const LOAN_OFFER_TEMPLATE = HANDLEBARS.compile(SOURCE);
+
+const LOAN_FORM_SOURCE = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Loan Calculator</title>
+    <style type="text/css">
+      body {
+        background: rgba(250, 250, 250);
+        font-family: sans-serif;
+        color: rgb(50, 50, 50);
+      }
+
+      article {
+        width: 100%;
+        max-width: 40rem;
+        margin: 0 auto;
+        padding: 1rem 2rem;
+      }
+
+      h1 {
+        font-size: 2.5rem;
+        text-align: center;
+      }
+
+      form,
+      input {
+        font-size: 1.5rem;
+      }
+      form p {
+        text-align: center;
+      }
+      label,
+      input {
+        display: block;
+        width: 100%;
+        padding: 0.5rem;
+        margin-top: 0.5rem;
+      }
+      input[type="submit"] {
+        width: auto;
+        margin: 1rem auto;
+        cursor: pointer;
+        color: #fff;
+        background-color: #01d28e;
+        border: none;
+        border-radius: 0.3rem;
+      }
+    </style>
+  </head>
+  <body>
+    <article>
+      <h1>Loan Calculator</h1>
+      <form action="/loan-offer" method="get">
+        <label for="APR"> what your apr? (if 5%, just write 5)</label>
+        <input type="APR" name="APR" value="">
+        <label for="amount">How much do you want to borrow (in dollars)?</label>
+        <input type="number" name="amount" value="">
+        <label for="amount">How much time do you want to pay back your loan?</label>
+        <input type="number" name="duration" value="">
+        <input type="submit" name="" value="Get loan offer!">
+      </form>
+    </article>
+  </body>
+</html>
+`;
+const LOAN_OFFER_TEMPLATE = HANDLEBARS.compile(LOAN_OFFER_SOURCE);
+const LOAN_FORM_TEMPLATE = HANDLEBARS.compile(LOAN_FORM_SOURCE);
 
 function render(template, data) {
   let html = template(data);
@@ -91,6 +159,11 @@ function render(template, data) {
 function getParams(path) {
   const myURL = new URL(path, `http://localHost:${PORT}`);
   return myURL.searchParams;
+}
+
+function getPathName(path) {
+  const myURL = new URL(path, `http://localhost:${PORT}`);
+  return myURL.pathname;
 }
 
 function calculatePayment(loanAmt, length, APR) {
@@ -119,19 +192,28 @@ function returnLoanInfo(params) {
   return data;
 }
 
+// eslint-disable-next-line max-statements
 const SERVER = HTTP.createServer((req, res) => {
-  // eslint-disable-next-line no-unused-vars
   let method = req.method;
   let path = req.url;
-  if (path === '/favicon.ico') {
-    res.statusCode = 400;
+  let pathname = getPathName(path);
+  if (path === '/') {
+    let content = render(LOAN_FORM_TEMPLATE);
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`${content}\n`);
     res.end();
-  } else {
+  } else if (pathname === `/loan-offer`) {
     let data = returnLoanInfo(getParams(path));
     let content = render(LOAN_OFFER_TEMPLATE, data);
+
     res.statusCode = 200;
-    res.setHeader('content-Type', 'text/html');
+    res.setHeader('Content-Type', 'text/html');
     res.write(`${content}\n`);
+    res.end();
+  } else {
+    res.statusCode = 404;
     res.end();
   }
 });
