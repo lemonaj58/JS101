fe8fe590c2363b0565fbd4cea95f4be2914158d9
lemonaj58 <lemonaj58@gmail.com>
2021-11-28 10:39:46 -0700

    refactoring pug and express

--- js175/express_pug_practice/app.js
+++ js175/express_pug_practice/app.js
@@ -1,11 +1,8 @@
 /* eslint-disable no-unused-vars */
 const express = require("express");
 const app = express();
+
 const morgan = require("morgan");
-const writeLog = (req, res) => {
-  let timeStamp = String(new Date()).substring(4,24);
-  console.log(`${timeStamp} ${req.method} ${req.originalUrl} ${res.statusCode}`);
-};
 
 app.set("views", "./views");
 app.set("view engine", "pug");
@@ -14,7 +11,14 @@ app.use(express.static("public"));
 app.use(morgan("common"));
 
 const showEnglishView = (req, res) => {
-  res.render("hello-world-english");
+  res.render("hello-world-english", {
+    currentPath: req.path,
+    language: "en-US"
+  });
+};
+
+app.locals.currentPathClass = (path, currentPath) => {
+  return path === currentPath ? "current" : "";
 };
 
 app.get("/", showEnglishView);
@@ -22,18 +26,24 @@ app.get("/", showEnglishView);
 app.get("/english", showEnglishView);
 
 app.get("/french", (req, res) => {
-  res.render("hello-world-french");
-  writeLog(req, res);
+  res.render("hello-world-french", {
+    currentPath: req.path,
+    language: "fr-FR"
+  });
 });
 
 app.get("/serbian", (req, res) => {
-  res.render("hello-world-serbian");
-  writeLog(req, res);
+  res.render("hello-world-serbian", {
+    currentPath: req.path,
+    language: "en-sr-Cyrl-rs"
+  });
 });
 
 app.get("/german", (req, res) => {
-  res.render("hello-world-german.pug");
-  writeLog(req, res);
+  res.render("hello-world-german.pug", {
+    currentPath: req.path,
+    language: "de-DE"
+  });
 });
 
 app.listen(3000, "localhost", () => {
